7dea704b207aee2354eced5b466a68feb185a489
Yi Li <leeoh12784@gmail.com>
2021-04-06 23:45:08 -0400

    hlscnn driver works for ila simulator offloading

--- hlscnn/conv_layer_driver.py
+++ hlscnn/conv_layer_driver.py
@@ -8,11 +8,14 @@ import numpy as np
 import subprocess
 import os
 import argparse
+import timeit
 
 from converter import Converter as cvtr
 
 class conv_layer_driver:
   __VIR_MEM_BASE_ADDR = 0x50000
+  __SPAD0_BASE_ADDR = 0x04000
+  __SPAD1_BASE_ADDR = 0X24000
 
   def __init__(self, inp_size, out_size, kernel_size, stride,
                is_bias, bias, is_relu, is_accum):
@@ -33,16 +36,17 @@ class conv_layer_driver:
     """
     this function produce vir_mem_wr asm for transferring weights and activations
     """
-    for i, data in enumerate(self.wgt_mem):
+    for i, data in enumerate(self.act_mem):
       self.ila_asm.append({
         'name' : 'VirMemWr',
         'addr' : hex(self.__VIR_MEM_BASE_ADDR + i*0x10),
         'data' : data
       })
-    for i, data in enumerate(self.act_mem):
+
+    for i, data in enumerate(self.wgt_mem):
       self.ila_asm.append({
         'name' : 'VirMemWr',
-        'addr' : hex(self.__VIR_MEM_BASE_ADDR + self.conv_act_offset + i*0x10),
+        'addr' : hex(self.__VIR_MEM_BASE_ADDR + self.conv_wgt_soc_offset + i*0x10),
         'data' : data
       })
   
@@ -50,7 +54,8 @@ class conv_layer_driver:
     """
     this function produce spad_wr asm for writing the weight data into spad
     """
-    # this may not work for HLSCNN systemc simulation when RdWrLen is larger than 1
+    # Warning: this may not work for HLSCNN systemc simulation when RdWrLen is larger than 1
+    # Warning: If to run hlscnn systemc simulation, we have to write to spad1 to initialize it to 0
     self.ila_asm.append({
       'name' : 'CfgSoCMemRdWrLen',
       'length' : len(self.wgt_mem)
@@ -58,11 +63,11 @@ class conv_layer_driver:
     # assume weight data starts from 0x0 in the SoC Memory (0x50000 for VirMem)
     self.ila_asm.append({
       'name' : 'CfgSoCMemBaseAddr',
-      'base_addr' : hex(0x0)
+      'base_addr' : hex(self.conv_wgt_soc_offset)
     })
     self.ila_asm.append({
       'name' : 'SpadWr',
-      'addr' : hex(0x04000)
+      'addr' : hex(self.__SPAD0_BASE_ADDR)
     })
 
   def produce_conv_layer_asm(self):
@@ -71,15 +76,15 @@ class conv_layer_driver:
     """
     self.ila_asm.append({
       'name' : 'CfgConvActBaseAddr',
-      'base_addr' : '0x00010000'
+      'base_addr' : hex(0x0)
     })
     self.ila_asm.append({
       'name' : 'CfgConvWgtBaseAddr',
-      'base_addr' : '0x00004000'
+      'base_addr' : hex(self.__SPAD0_BASE_ADDR)
     })
     self.ila_asm.append({
       'name' : 'CfgConvOutBaseAddr',
-      'base_addr' : '0x00024000'
+      'base_addr' : hex(self.__SPAD1_BASE_ADDR)
     })
     """
     Conv layer parameters
@@ -120,7 +125,23 @@ class conv_layer_driver:
     """
     produce asm for reading data from HLSCNN
     """
-    pass
+    # reference from conv_test_standalone in host.h by Harvard
+    # out_idx is the idx of the output element in the output matrix
+    self.out_addr = [] # holding the address of the output element
+    
+    for n in range(self.k_num):
+      for i in range(self.out_rows):
+        for j in range(self.out_cols):
+          in_row = int(i*self.k_r_stride + (self.kernel_rows-1)/2)
+          in_col = int(j*self.k_c_stride + (self.kernel_cols-1)/2)
+          spad_idx = n*self.inp_rows*self.inp_cols + in_row*self.inp_cols + in_col
+          spad_addr = spad_idx * 0x10 + self.__SPAD1_BASE_ADDR
+          # append the read instructions
+          self.ila_asm.append({
+            'name' : 'SpadRd',
+            'addr' : hex(spad_addr)
+          })
+          self.out_addr.append(spad_addr + ((self.k_num-1)%8) * 2)
   
   def produce_asm_all(self):
     self.produce_vir_mem_wr_asm()
@@ -135,7 +156,7 @@ class conv_layer_driver:
     with open('./test/conv_ila_data_lib.json', 'w') as fout:
       json.dump(self.data_lib, fout, indent=2)
 
-  def collect_data(self):
+  def collect_data_in(self):
     """
     collect relay data from files
     """
@@ -152,7 +173,8 @@ class conv_layer_driver:
     with open('./data/packed_conv_wgt.json', 'r') as fin:
       self.wgt_mem = json.load(fin)
 
-    self.conv_act_offset = (len(self.wgt_mem) + 1) * 0x10
+    # self.conv_act_offset = (len(self.wgt_mem) + 1) * 0x10
+    self.conv_wgt_soc_offset = int(len(self.act_mem)/16 + 1) * 16 * 0x10
 
   def produce_prog_frag(self):
     print('\n--------------------------------------------------------------')
@@ -161,11 +183,37 @@ class conv_layer_driver:
     self.ila_cvtr = cvtr('./test/conv_ila_asm.json', './test/conv_ila_data_lib.json')
     self.ila_cvtr.dump_ila_prog_frag('./test/conv_ila_prog_frag.json')
   
+  def invoke_ila_simulator(self):
+    print('\n--------------------------------------------------------------')
+    print('\tinvoking ILA simulator')
+    print('--------------------------------------------------------------\n')
+    # measure the time of ila simulation
+    cmd = ['hlscnn_asm_sim_driver', './test/conv_ila_prog_frag.json', './test/conv_ila_out.json']
+    start_time = timeit.default_timer()
+    subprocess.run(cmd)
+    end_time = timeit.default_timer()
+    print('\n********* ILA simulator performance ***********')
+    print('ILA simulator execution time is {:04f}s'.format(end_time - start_time))
+  
+  def collect_ila_result(self):
+    print('\n--------------------------------------------------------------')
+    print('\tcollecting ILA simulation result')
+    print('--------------------------------------------------------------\n')
+    with open('./test/conv_ila_out.json', 'r') as fin:
+      ila_out = json.load(fin)
+      result = []
+      for addr in self.out_addr:
+        key = f'0x{addr:08X}'
+        result.append(ila_out[key])
+    np.asarray(result).tofile('./data/conv_result.txt', sep='\n')
+
   def run(self):
     subprocess.run(['mkdir', '-p', 'test', 'data'])
-    self.collect_data()
+    self.collect_data_in()
     self.produce_asm_all()
     self.produce_prog_frag()
+    self.invoke_ila_simulator()
+    self.collect_ila_result()
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='ConvLayer Parameters')
